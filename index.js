bf62be3ef3d97cac2f59b7a4a87fb11773242e0f
RudraPrasad001 <rudraprasad.email@gmail.com>
2024-11-30 17:37:15 +0530

    Final Commit

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		  entry.target.classList.add('in-view');
 		  return;
 		}
-		entry.target.classList.remove('in-view');
 	  });
 	});
 	const allAnimatedElements = document.querySelectorAll('.animate');
